18e8a39f6864fdb9941a36f31b0d468bf69d5667
Mabena490 <Mabenanaphty@gmail.com>
2023-05-12 03:10:43 -0700

    Tasks

--- helper_functions.c
+++ helper_functions.c
@@ -8,7 +8,7 @@
  */
 int line_validator(char *str)
 {
-  int index = 0, flag = 0, len = 0;
+int index = 0, flag = 0, len = 0;
 
 len = strlen(str);
 while (str[index] == ' ' || str[index] == '\t' || str[index] == '\n')
@@ -19,7 +19,7 @@ index++;
 if (index == len)
 flag = 1;
 
-  return (flag);
+return (flag);
 }
 
 /**
@@ -33,32 +33,32 @@ void is_digit(char *number)
   unsigned int i = 0;
 
   if (number == NULL)
-    {
-      fprintf(stderr, "L%u: usage: push integer\n", var.n_lines);
-      free(var.getl_info);
-      handle_dlist_head(var.stack_head);
-      fclose(var.fp_struct);
-      exit(EXIT_FAILURE);
-    }
-  else if (number[0] != '-' && (number[0] < 48 || number[0] > 57))
-    {
-      fprintf(stderr, "L%u: usage: push integer\n", var.n_lines);
-      free(var.getl_info);
-      handle_dlist_head(var.stack_head);
-      fclose(var.fp_struct);
-      exit(EXIT_FAILURE);
-    }
-  for (i = 1; number[i] != '\0'; i++)
-    {
-      if (number[i] < 48 || number[i] > 57)
-	{
-	  fprintf(stderr, "L%u: usage: push integer\n", var.n_lines);
-	  free(var.getl_info);
-	  handle_dlist_head(var.stack_head);
-	  fclose(var.fp_struct);
-	  exit(EXIT_FAILURE);
-	}
-    }
+{
+fprintf(stderr, "L%u: usage: push integer\n", var.n_lines);
+free(var.getl_info);
+handle_dlist_head(var.stack_head);
+fclose(var.fp_struct);
+exit(EXIT_FAILURE);
+}
+else if (number[0] != '-' && (number[0] < 48 || number[0] > 57))
+{
+fprintf(stderr, "L%u: usage: push integer\n", var.n_lines);
+free(var.getl_info);
+handle_dlist_head(var.stack_head);
+fclose(var.fp_struct);
+exit(EXIT_FAILURE);
+}
+for (i = 1; number[i] != '\0'; i++)
+{
+if (number[i] < 48 || number[i] > 57)
+{
+fprintf(stderr, "L%u: usage: push integer\n", var.n_lines);
+free(var.getl_info);
+handle_dlist_head(var.stack_head);
+fclose(var.fp_struct);
+exit(EXIT_FAILURE);
+}
+}
 }
 
 /**
@@ -69,16 +69,16 @@ void is_digit(char *number)
  */
 size_t dlistint_len(stack_t *h)
 {
-  size_t n_nodes = 0;
+size_t n_nodes = 0;
 
-  if (h == NULL)
-    {
-      return (0);
-    }
-  while (h != NULL)
-    {
-      h = h->next;
-      n_nodes++;
-    }
-  return (n_nodes);
+if (h == NULL)
+{
+return (0);
+}
+while (h != NULL)
+{
+h = h->next;
+n_nodes++;
+}
+return (n_nodes);
 }
